cd61b554d0c6a0d4ff41de0bf0ec96ba08f07a6d
Brinkhuis <rene.brinkhuis@gmail.com>
2017-11-12 18:25:05 +0100

    Plot 'Price Distribution per Operating System' added

--- code/visualize_data.py
+++ code/visualize_data.py
@@ -34,3 +34,25 @@ sns.boxplot(x='price',
             width=0.75).set_title('Price Distribution per Storage Size')
 sns.despine(offset=10, trim=True)
 plt.savefig('plots/price_distribution_storage.png')
+
+# plot price distribution per operation system
+X_PIXELS, Y_PIXELS, DPI = 1500, 1000, 150
+X_INCH, Y_INCH = X_PIXELS / DPI, Y_PIXELS / DPI
+plt.figure(figsize=(X_INCH, Y_INCH), dpi=DPI)
+sns.boxplot(x='price',
+            y='os',
+            data=PRODUCTINFO[PRODUCTINFO.os.notnull()].groupby('os').filter(lambda x: len(x) > 5),
+            order=['Android 5.0',
+                   'Android 5.1',
+                   'Android 6.0',
+                   'Android 7.0',
+                   'Android 7.1',
+                   'Android 8.0',
+                   'iOS 9',
+                   'iOS 9.3',
+                   'iOS 10',
+                   'iOS 11'],
+            palette='PRGn',
+            width=0.75).set_title('Price Distribution per Operating System')
+sns.despine(offset=10, trim=True)
+plt.savefig('plots/price_distribution_os.png')
