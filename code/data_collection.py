44508820de979878a8c6e8bc31757fa93c559664
Brinkhuis <rene.brinkhuis@gmail.com>
2018-11-02 12:14:57 +0100

    Issues due to new webpage structure fixed

--- code/data_collection.py
+++ code/data_collection.py
@@ -5,62 +5,79 @@ Scraping price information from www.coolblue.nl
 import pandas as pd
 import requests
 from bs4 import BeautifulSoup
+from tqdm import tqdm
 
-URL = 'https://www.coolblue.nl/producttype:mobiele-telefoons'
+BASE_URL = 'https://www.coolblue.nl/producttype:mobiele-telefoons'
 
-def npages(mysoup):
+
+def npages(url):
     """
-    This functions returns the number of pages of URL.
+    Return number of subpages from base url
     """
+
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
     pagination = list()
-    for page_number in mysoup.find_all('a', {'class': 'pagination__content'}):
+    for page_number in soup.find_all('span', {'class': 'pagination__link-text'}):
         pagination.append(page_number.text.strip())
     return int(pagination[-2])
 
-PRODUCT = list()
-PRICE = list()
-RATING = list()
-REVIEWS = list()
-HIGHLIGHT1 = list()
-HIGHLIGHT2 = list()
-HIGHLIGHT3 = list()
 
-for page in range(1, npages(BeautifulSoup(requests.get(URL).text,
-                                          'html.parser')) + 1):
-    soup = BeautifulSoup(requests.get(URL + '?pagina=' + str(page)).text,
-                         'html.parser')
+def get_productinfo(url):
+    """
+    Scrape product information per webpage
+    """
+    
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, 'html.parser')
+    
+    product_list = list()
+    price_list = list()
+    rating_list = list()
+    review_list = list()
+    
     products = soup.find_all('a', {'class': 'product__title js-product-title'})
     for product in products:
-        PRODUCT.append(product.text.strip())
+        product_list.append(product.text.strip())
+    
     prices = soup.find_all('strong', {'class': 'product__sales-price'})
     for price in prices:
-        PRICE.append(float(price.text.strip().strip(',-')
-                           .replace('.', '').replace(',', '.')))
+        price_list.append(float(price.text.strip().strip(',-').replace('.', '').replace(',', '.')))
+    
     ratings = soup.find_all('meter', {'class': 'review-rating--score-meter'})
     for rating in ratings:
-        RATING.append(str(rating).split('value="')[1].split('">')[0])
+        rating_list.append(str(rating).split('value="')[1].split('">')[0])
+    
     reviews = soup.find_all('span', {'class': 'review-rating--reviews'})
     for review in reviews:
-        REVIEWS.append(int(review.text.strip().strip(' reviews')))
-    highlights = soup.find_all('li', {'class': 'product__highlight'})
-    highlight_counter = 1
-    for highlight in highlights:
-        if highlight_counter == 1:
-            HIGHLIGHT1.append(str(highlight).strip('</li>').split('>')[-1])
-        if highlight_counter == 2:
-            HIGHLIGHT2.append(str(highlight).strip('</li>').split('>')[-1])
-        if highlight_counter == 3:
-            HIGHLIGHT3.append(str(highlight).strip('</li>').split('>')[-1])
-        highlight_counter += 1
-        if highlight_counter > 3:
-            highlight_counter = 1
+        review_list.append(int(review.text.strip().strip(' reviews')))
 
-PRODUCTINFO = pd.DataFrame({'product': PRODUCT,
-                            'price': PRICE,
-                            'rating': RATING,
-                            'reviews': REVIEWS,
-                            'highlight1': HIGHLIGHT1,
-                            'highlight2': HIGHLIGHT2,
-                            'highlight3': HIGHLIGHT3})
+    productinfo = pd.DataFrame({'product': product_list,
+                                'price': price_list,
+                                'rating': rating_list,
+                                'reviews': review_list})
+    return productinfo
 
-PRODUCTINFO.to_csv('data/productinfo_raw.csv', index=False)
+
+def main():
+    """
+    Save scraped product data to file
+    """
+    
+    df = pd.DataFrame()
+
+    for page in tqdm(range(1, npages(BASE_URL) + 1)):
+        data = get_productinfo(f'{BASE_URL}?pagina={page}')
+        df = pd.concat([df, data])
+    
+    df['brand'] = df['product'].apply(lambda x: x.strip().split(' ')[0])
+    
+    file_name = '../data/productinfo.csv'
+    
+    df.to_csv(file_name, index=False)
+    
+    print(f'{df.shape[0]} records saved to file {file_name}')
+    
+if __name__ == "__main__":
+    main()
--- code/data_visualization.py
+++ code/data_visualization.py
@@ -8,7 +8,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # reading data
-PRODUCTINFO = pd.read_csv('data/productinfo_prep.csv')
+PRODUCTINFO = pd.read_csv('../data/productinfo.csv')
 
 # set plot dimensions
 X_PIXELS, Y_PIXELS, DPI = 1500, 1000, 150
@@ -24,16 +24,6 @@ sns.boxplot(x='price',
             palette='PRGn',
             width=0.75).set_title('Price Distribution per Brand')
 sns.despine(offset=10, trim=True)
-plt.savefig('plots/price_distribution_brand.png')
 
-# plot price distribution per operation system
-plt.figure(figsize=(X_INCH, Y_INCH), dpi=DPI)
-sns.boxplot(x='price',
-            y='os',
-            data=PRODUCTINFO.groupby('os').filter(lambda x: len(x) > 5),
-            order=list(PRODUCTINFO.groupby('os').filter(lambda x: len(x) > 5)
-                       .groupby('os').price.median().sort_values(ascending=False).index),
-            palette='PRGn',
-            width=0.75).set_title('Price Distribution per Operating System')
-sns.despine(offset=10, trim=True)
-plt.savefig('plots/price_distribution_os.png')
+# save plot
+plt.savefig('../plots/price_distribution_brand.png')
