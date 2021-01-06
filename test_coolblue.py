7ef66619cd544279e12d3ea678b2a12f76ba96be
René Brinkhuis <rene.brinkhuis@gmail.com>
2021-01-06 09:25:39 +0100

    Fixes #5

--- test_coolblue.py
+++ test_coolblue.py
@@ -9,3 +9,4 @@ def test_get_productinfo():
     url = 'https://www.coolblue.nl/producttype:mobiele-telefoons'
     productinfo = data_collection.get_productinfo(url + '?pagina={1}')
     assert productinfo.shape[0] > 0, "Should be greater than 0"
+
