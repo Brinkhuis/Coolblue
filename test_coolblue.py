7da03a0c11a03b6d11bbce8710e6728fa0153a0b
René Brinkhuis <rene.brinkhuis@gmail.com>
2021-01-06 09:22:46 +0100

    Fixes #4

---
+++ test_coolblue.py
@@ -0,0 +1,11 @@
+import data_collection
+
+def test_npages():
+    url = 'https://www.coolblue.nl/producttype:mobiele-telefoons'
+    n = data_collection.npages(url)
+    assert type(n) == int, "Should be True"
+
+def test_get_productinfo():
+    url = 'https://www.coolblue.nl/producttype:mobiele-telefoons'
+    productinfo = data_collection.get_productinfo(url + '?pagina={1}')
+    assert productinfo.shape[0] > 0, "Should be greater than 0"
